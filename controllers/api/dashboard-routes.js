813edf46f13138d432f2ad2ec88f80ab25f0055a
Joaquin Florencio <Jrr232@gmail.com>
2023-09-10 17:47:43 -0400

    comments

--- controllers/api/dashboard-routes.js
+++ controllers/api/dashboard-routes.js
@@ -51,11 +51,27 @@ router.put('/:id', async (req, res) => {
             }
         );
 
-        res.render('dashboard'); // Render the 'dashboard' view after successful update
+        // res.render('dashboard'); // Render the 'dashboard' view after successful update
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error'); // Handle errors appropriately, this is just an example
     }
 });
+
+router.delete('/:id', (req, res) => {
+    // Looks for the books based on isbn given in the request parameters and deletes the instance from the database
+    Blog.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => res.json(err));
+});
+
+
+
 
 module.exports = router;

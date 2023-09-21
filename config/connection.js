6fea5a374824b9cc3f2c6d99a8d99667f070134e
Joaquin Florencio <Jrr232@gmail.com>
2023-09-21 13:06:08 -0400

    just added jawsdb .env

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 let sequelize = "";
+// new jawsDB
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize("mysql://d1vp3zq1wm2vvo3u:u6tdp7oyz7had3mn@wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/q5sfkt1m8sszorv4");
 } else {
